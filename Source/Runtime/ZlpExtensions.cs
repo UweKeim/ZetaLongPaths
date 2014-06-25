642a494a122d6349cf6fa3a361cf9d9d877714a1
Uwe Keim <uwe.keim@gmail.com>
2014-06-25 14:12:00 +0200

    .NET 4, Extension methods
    Migrated to .NET 4 (from .NET 2). Added some extension methods

---
+++ Source/Runtime/ZlpExtensions.cs
@@ -0,0 +1,57 @@
+﻿namespace ZetaLongPaths
+{
+    public static class ZlpExtensions
+    {
+        public static ZlpDirectoryInfo CombineDirectory(this ZlpDirectoryInfo one, ZlpDirectoryInfo two)
+        {
+            if (one == null) return two;
+            else if (two == null) return one;
+
+            else return new ZlpDirectoryInfo(ZlpPathHelper.Combine(one.FullName, two.FullName));
+        }
+
+        public static ZlpDirectoryInfo CombineDirectory(this ZlpDirectoryInfo one, string two)
+        {
+            if (one == null && two == null) return null;
+            else if (two == null) return one;
+            else if (one == null) return new ZlpDirectoryInfo(two);
+
+            else return new ZlpDirectoryInfo(ZlpPathHelper.Combine(one.FullName, two));
+        }
+
+        public static ZlpDirectoryInfo CombineDirectory(/*this*/ string one, string two)
+        {
+            if (one == null && two == null) return null;
+            else if (two == null) return new ZlpDirectoryInfo(one);
+            else if (one == null) return new ZlpDirectoryInfo(two);
+
+            else return new ZlpDirectoryInfo(ZlpPathHelper.Combine(one, two));
+        }
+
+        public static ZlpFileInfo CombineFile(this ZlpDirectoryInfo one, ZlpFileInfo two)
+        {
+            if (one == null) return two;
+            else if (two == null) return null;
+
+            else return new ZlpFileInfo(ZlpPathHelper.Combine(one.FullName, two.FullName));
+        }
+
+        public static ZlpFileInfo CombineFile(this ZlpDirectoryInfo one, string two)
+        {
+            if (one == null && two == null) return null;
+            else if (two == null) return null;
+            else if (one == null) return new ZlpFileInfo(two);
+
+            else return new ZlpFileInfo(ZlpPathHelper.Combine(one.FullName, two));
+        }
+
+        public static ZlpFileInfo CombineFile(/*this*/ string one, string two)
+        {
+            if (one == null && two == null) return null;
+            else if (two == null) return null;
+            else if (one == null) return new ZlpFileInfo(two);
+
+            else return new ZlpFileInfo(ZlpPathHelper.Combine(one, two));
+        }
+    }
+}
