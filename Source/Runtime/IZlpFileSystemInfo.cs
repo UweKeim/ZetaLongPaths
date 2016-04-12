602e2e4a13144b68975dc549014159fd50141989
Uwe Keim <uwe.keim@gmail.com>
2016-04-12 07:16:19 +0200

    White-space

--- Source/Runtime/IZlpFileSystemInfo.cs
+++ Source/Runtime/IZlpFileSystemInfo.cs
@@ -1,30 +1,21 @@
 namespace ZetaLongPaths
 {
     using System;
-    using ZetaLongPaths.Native;
+    using Native;
 
     public interface IZlpFileSystemInfo
     {
         bool Exists { get; }
-
         string OriginalPath { get; }
-
         string FullName { get; }
-
         string Name { get; }
-
         DateTime LastWriteTime { get; set; }
-
         DateTime LastAccessTime { get; set; }
-
         DateTime CreationTime { get; set; }
-
         FileAttributes Attributes { get; set; }
 
         void MoveToRecycleBin();
-
         string ToString();
-
         void MoveTo(string destinationDirectoryPath);
     }
 }
