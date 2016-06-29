42ca13070da7fd36e0091de582e81c7f60b6eb92
Uwe Keim <uwe.keim@gmail.com>
2016-06-29 15:16:45 +0200

    Bumped version to 1.0.0.11

--- Source/Runtime/Properties/AssemblyInfo.cs
+++ Source/Runtime/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zeta Software GmbH")]
 [assembly: AssemblyProduct("Zeta Long Paths")]
-[assembly: AssemblyCopyright("© 2009-2015 Zeta Software GmbH")]
+[assembly: AssemblyCopyright("© 2009-2016 Zeta Software GmbH")]
 [assembly: AssemblyTrademark("http://www.zeta-producer.com")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.10")]
-[assembly: AssemblyFileVersion("1.0.0.10")]
+[assembly: AssemblyVersion("1.0.0.11")]
+[assembly: AssemblyFileVersion("1.0.0.11")]
