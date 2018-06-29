36281bc6c6e23f9a70e00a89aeac920cb65ddd98
Uwe Keim <uwe.keim@gmail.com>
2018-06-29 09:45:18 +0200

    NuGet update

--- Source/RuntimeNetCore/ZlpSimpleFileAccessProtectorException.cs
+++ Source/RuntimeNetCore/ZlpSimpleFileAccessProtectorException.cs
@@ -20,5 +20,10 @@
         public ZlpSimpleFileAccessProtectorException(string message, Exception inner) : base(message, inner)
         {
         }
+
+        //public override string Message =>
+        //    string.IsNullOrEmpty(InnerException?.Message)
+        //        ? base.Message
+        //        : $@"{InnerException.Message} ({base.Message})";
     }
 }
